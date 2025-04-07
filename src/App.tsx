0c83d28eaa97fce3793772d953d28c220c4bcd77
HAO-317 <lihao317@gmail.com>
2025-04-07 15:41:37 +0200

    fix display issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BlogPost from "./pages/BlogPost";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL || "/"}> 
       <div>
         <Navbar />
         <Routes>
